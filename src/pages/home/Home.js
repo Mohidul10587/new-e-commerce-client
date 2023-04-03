9c2bd3dcc19017cd2e58a0e87a9244504bd54432
mohid <mohid10587@gmail.com>
2023-04-03 12:22:29 +0600

    problem

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -13,28 +13,7 @@ const Home = () => {
 
   return (
     <div>
-      {/* <div style={{ backgroundImage: "url(/b.jpeg)" }} className='h-screen bg-cover italic'>
-        <div className='h-full w-full bg-gray-900 bg-opacity-50 flex justify-center items-center'>
-
-          <div className='w-1/2'>
-            <p className='font bold md:text-4xl text-2xl text-white'>Welcome to...</p>
-            <p className='font bold md:text-7xl text-4xl text-white mt-4'>
-              <span className='text-pink-500'>S</span>hopping <span className='text-pink-500'>W</span>orld</p>
-
-            <p className='font bold text-2xl text-white mt-4 hidden md:block'> Biker’s Warehouse is one of Johannesburg’s top bike and accessory brand suppliers. Our brands are carefully selected to offer the widest product range to cater for the needs of any road, dual-sport or off-road enthusiast. </p>
-          </div>
-        </div>
-      </div> */}
-
-
-
-<Banner/>
-
-
-
-
-
-
+      <Banner />
       <h1 className='font-bold text-3xl text-center mt-10'>Brows by category</h1>
 
       {categories?.map((category, index) => <div className='mt-10' key={index}>
